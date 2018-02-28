93b41de0fa1c0fe922f18d77916b74e3a9a4ede6
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-02-28 22:58:18 +0100

    get characters from database

--- people.js
+++ people.js
@@ -13,13 +13,13 @@ module.exports = function(){
         });
     }
 
-    function getPeople(res, mysql, context, complete){
-        mysql.pool.query("SELECT bsg_people.id, fname, lname, bsg_planets.name AS homeworld, age FROM bsg_people INNER JOIN bsg_planets ON homeworld = bsg_planets.id", function(error, results, fields){
+    function getCharacters(res, mysql, context, complete){
+        mysql.pool.query("SELECT got_character.id, got_character.first_name, got_character.last_name FROM got_character", function(error, results, fields){
             if(error){
                 res.write(JSON.stringify(error));
                 res.end();
             }
-            context.people = results;
+            context.characters = results;
             complete();
         });
     }
@@ -44,7 +44,7 @@ module.exports = function(){
         var context = {};
         context.jsscripts = ["deleteperson.js"];
         var mysql = req.app.get('mysql');
-        getPeople(res, mysql, context, complete);
+        getCharacters(res, mysql, context, complete);
         getPlanets(res, mysql, context, complete);
         function complete(){
             callbackCount++;
