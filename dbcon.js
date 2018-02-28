8f886b817ea9b6ad5360e79840a8c44b9407bac0
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-02-28 22:02:20 +0100

    change db login instructions

--- dbcon.js
+++ dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_wolfordj',
-  password        : '7235',
-  database        : 'cs340_wolfordj'
+  user            : 'cs340_middlebs',
+  password        : '6387',
+  database        : 'cs340_middlebs'
 });
 module.exports.pool = pool;
